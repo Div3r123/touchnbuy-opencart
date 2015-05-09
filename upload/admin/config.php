e3d98ff72de440c7cbb761ea44721a04525241d2
Dawid <dawid@dawidmazurek.pl>
2015-05-09 17:41:49 +0200

    initial commit

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://touchnbuy-opencart/admin/');
+define('HTTP_CATALOG', 'http://touchnbuy-opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://touchnbuy-opencart/admin/');
+define('HTTPS_CATALOG', 'http://touchnbuy-opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/admin/');
+define('DIR_SYSTEM', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/');
+define('DIR_LANGUAGE', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/admin/language/');
+define('DIR_TEMPLATE', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/admin/view/template/');
+define('DIR_CONFIG', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/config/');
+define('DIR_IMAGE', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/image/');
+define('DIR_CACHE', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/download/');
+define('DIR_UPLOAD', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/upload/');
+define('DIR_LOGS', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/logs/');
+define('DIR_MODIFICATION', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/system/modification/');
+define('DIR_CATALOG', '/var/www/paynbuy/opencart/opencart-2.0.2.0/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart123');
+define('DB_DATABASE', 'touchnbuy-opencart');
+define('DB_PREFIX', 'oc_');
